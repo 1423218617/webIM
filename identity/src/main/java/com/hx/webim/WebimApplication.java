6d7674f208d9f41d0f541bfe60e277aaee019b47
Administrator <>
2019-10-04 10:39:11 +0800

    删除没用文件

---
+++ identity/src/main/java/com/hx/webim/WebimApplication.java
@@ -0,0 +1,13 @@
+package com.hx.webim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebimApplication.class, args);
+    }
+
+}
