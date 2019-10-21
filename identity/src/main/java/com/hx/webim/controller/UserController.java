d678713e4a4a0ba3f2f6628b2f1997bbf751e701
user.name <1423218617@qq.com>
2019-10-21 13:52:25 +0800

    文件上传

--- identity/src/main/java/com/hx/webim/controller/UserController.java
+++ identity/src/main/java/com/hx/webim/controller/UserController.java
@@ -12,9 +12,14 @@ import com.hx.webim.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.mail.Multipart;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -87,6 +92,7 @@ public class UserController {
     }
 
     @GetMapping("groupMembers/{gid}")
+    @ResponseBody
     public Object groupMembers(@PathVariable Integer gid){
         List<User> memberList=userService.findGroupMembersByGid(gid);
         GroupMemberInfo groupMemberInfo=new GroupMemberInfo();
@@ -96,7 +102,14 @@ public class UserController {
         resultVo.setMsg("群成员信息");
         resultVo.setData(groupMemberInfo);
         return resultVo;
+    }
 
-
+    @PostMapping("uploadImage")
+    @ResponseBody
+    public Object uploadImage(@RequestParam("file") MultipartFile file)throws IOException {
+        FileOutputStream fileOutputStream=new FileOutputStream(new File("uploadfile/"+file.getOriginalFilename()));
+        fileOutputStream.write(file.getBytes());
+        System.out.println(file.getSize());
+        return new ResultVo<>();
     }
 }
